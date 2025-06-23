ddc36b1b01e2b9e4aa2b1654835e5f94091ba835
Seii <seii.tanomoshii@gmail.com>
2025-06-23 11:35:04 -0700

    Add ability to look up specific verses

--- README.md
+++ README.md
@@ -1,14 +1,19 @@
 ## maubot-bible
-A simple [maubot](https://github.com/maubot/maubot) module for Matrix servers that responds with a random Bible verse (currently sourced from [bible-api.com](https://bible-api.com)).
+A simple [maubot](https://github.com/maubot/maubot) module for Matrix servers that responds with a Bible verse (currently sourced from [bible-api.com](https://bible-api.com)).
 
 ## Usage
 The trigger can be user-defined, but by default it is `!verse`.
+
+To request a random verse, use `!verse random`.
+To request a specific verse, use `!verse <book> <chapter>:<verse>` (case-insensitive)
+
+Example: `!verse 1 corinthians 3:16`
 
 ## Installation
 This plugin can be installed using the standard [maubot plugin instructions](https://docs.mau.fi/maubot/usage/basic.html#uploading-plugins).
 
 ## Functionality
-Currently this plugin only attempts to provide a random verse. `bible-api` does provide many options to do specific look ups or return other translations, but they have not been implemented for this plugin at this time.
+This plugin defaults to King James Version translations due to 2 different APIs being consumed. Currently it is possible to request either a random verse, or a specific verse. `bible-api` provides several options to do specific look ups or return other translations, but they are not implemented at this time.
 
 ## Credits
 Many thanks to `williamkray`, as this bot is based heavily on the code from their stock ticker plugin [maubot-ticker](https://github.com/williamkray/maubot-ticker).
--- maubot_bible/biblebot.py
+++ maubot_bible/biblebot.py
@@ -9,7 +9,7 @@ import json
 
 class Config(BaseProxyConfig):
     def do_update(self, helper: ConfigUpdateHelper) -> None:
-        helper.copy("randomTrigger")
+        helper.copy("baseTrigger")
         helper.copy("baseApi")
         helper.copy("randomPath")
 
@@ -22,12 +22,16 @@ class BibleBot(Plugin):
     def get_config_class(cls) -> Type[BaseProxyConfig]:
         return Config
 
-    @command.new(name=lambda self: self.config["randomTrigger"],
+    @command.new(name=lambda self: self.config["baseTrigger"],
             help="Request a random Bible verse reference")
-    async def random_verse_handler(self, evt: MessageEvent) -> None:
+    @command.argument("verse", pass_raw=True, required=True)
+    async def verse_handler(self, evt: MessageEvent, verse: str) -> None:
         await evt.mark_read()
         
-        url = f"https://{self.config["baseApi"]}/{self.config["randomPath"]}"
+        if(verse.lower() == "random"):
+            url = f"https://{self.config["baseApi"]}/{self.config["randomPath"]}"
+        else:
+            url = f"https://{self.config["baseApi"]}/{verse.lower()}"
         
         try:
             response = await self.http.get(url)
@@ -36,11 +40,19 @@ class BibleBot(Plugin):
             await evt.respond(f"request failed: {e.message}")
             return None
         try:
-            book = resp_json["random_verse"]["book"]
-            chapter = resp_json["random_verse"]["chapter"]
-            verse = resp_json["random_verse"]["verse"]
-            text = resp_json["random_verse"]["text"]
-            translation = resp_json["translation"]["name"]
+            if(verse.lower() == "random"):
+                book = resp_json["random_verse"]["book"]
+                chapter = resp_json["random_verse"]["chapter"]
+                verse = resp_json["random_verse"]["verse"]
+                text = resp_json["random_verse"]["text"]
+                translation = resp_json["translation"]["name"]
+            else:
+                book = resp_json["verses"][0]["book_name"]
+                chapter = resp_json["verses"][0]["chapter"]
+                verse = resp_json["verses"][0]["verse"]
+                text = resp_json["verses"][0]["text"]
+                translation = resp_json["translation_names"]
+            
             bibleResponse = BibleResponse(book, chapter, verse, text, translation)
             verse_resp = bibleResponse.randomVerse()
         except Exception as e:
