c82ed82db58e879ab72f9b3424a2ecdc6ef5c4be
Seii <seii.tanomoshii@gmail.com>
2025-06-23 10:15:29 -0700

    Another attempt

--- biblebot.py
+++ biblebot.py
@@ -3,7 +3,7 @@ from typing import Optional, Type
 from mautrix.util.config import BaseProxyConfig, ConfigUpdateHelper
 from maubot import Plugin, MessageEvent
 from maubot.handlers import command
-from bibleresponse import BibleResponse
+from bibleresponse import *
 
 import json
 
